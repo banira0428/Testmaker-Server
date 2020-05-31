bb1b806c657e45479d506b07d600688d6e391025
山田敬汰 <banira0428@gmail.com>
2020-05-31 22:48:10 +0900

    fix main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+mod filter;
+
+#[tokio::main]
+async fn main() {
+
+    let database = db::init_db();
+
+    warp::serve(filter::tests_api(database)).run(([127, 0, 0, 1], 3030)).await;
 }
