41eccb0627b716c7b9e8b2ee810008a0aeed3a37
山田敬汰 <banira0428@gmail.com>
2020-05-31 21:33:37 +0900

    define model

---
+++ src/models.rs
@@ -0,0 +1,11 @@
+#[derive(Clone, Debug)]
+struct Test {
+    id: u64,
+    name: String,
+    color: u8,
+    document_id: String,
+    size: u64,
+    comment: String,
+    user_id: String,
+    user_name: String
+}
