b5abc8d85a0d4858fd8fc51c590ca752273fa3c2
山田敬汰 <banira0428@gmail.com>
2020-06-03 15:25:20 +0900

    connect db

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,24 @@
 use std::collections::HashMap;
 use std::sync::Arc;
-
 use tokio::sync::Mutex;
 
 use super::models::Test;
 
-pub type Database = Arc<Mutex<HashMap<u64, Test>>>;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub type Database = Arc<Mutex<HashMap<i32, Test>>>;
 
 pub fn init_db() -> Database {
     Arc::new(Mutex::new(HashMap::new()))
+}
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
